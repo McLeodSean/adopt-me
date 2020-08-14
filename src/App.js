fb30048575bef5a5609fb1a6688438402a8077d8
Sean <seanmcleod93@gmail.com>
2020-08-14 16:36:37 +1000

    Create custom hook for animal & breed dropdowns

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  // return React.createElement("div", { id: "hello" }, [
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, {
-  //     name: "Luna",
-  //     animal: "Dog",
-  //     breed: "Havanese",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Greg",
-  //     animal: "Bird",
-  //     breed: "Cockatoo",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Doink",
-  //     animal: "Cat",
-  //     breed: "Tabby",
-  //   }),
-  // ]);
-
   return (
     <div>
       <h1 id="something-important">Adopt Me</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Greg" animal="Bird" breed="Cockatoo" />
-      <Pet name="Finn" animal="Dog" breed="Border Collie" />
+      <SearchParams />
     </div>
   );
 };

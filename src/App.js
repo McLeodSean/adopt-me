15bf0f0f99a43d743584a109ba2ac2a053205fbd
Sean <seanmcleod93@gmail.com>
2020-08-14 11:41:29 +1000

    Convert pure react to JSX

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", { id: "hello" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Greg",
-      animal: "Bird",
-      breed: "Cockatoo",
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Tabby",
-    }),
-  ]);
+  // return React.createElement("div", { id: "hello" }, [
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, {
+  //     name: "Luna",
+  //     animal: "Dog",
+  //     breed: "Havanese",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Greg",
+  //     animal: "Bird",
+  //     breed: "Cockatoo",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Doink",
+  //     animal: "Cat",
+  //     breed: "Tabby",
+  //   }),
+  // ]);
+
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Greg" animal="Bird" breed="Cockatoo" />
+      <Pet name="Finn" animal="Dog" breed="Border Collie" />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
